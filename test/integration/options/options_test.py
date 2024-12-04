52e8030640e18e58db30453e9def53779684ddc2
Abril Rincón Blanco <git@rinconblanco.es>
2024-12-04 12:37:20 +0100

    Fix

--- test/integration/options/options_test.py
+++ test/integration/options/options_test.py
@@ -778,10 +778,10 @@ class TestConflictOptionsWarnings:
 def test_get_safe_none_option_checks():
     tc = TestClient(light=True)
     tc.save({"conanfile.py": GenConanfile("foo", "1.0")
-            .with_package("self.output.info(f'get_safe is None: {self.options.get_safe('myoption') is None}')",
-                          "self.output.info(f'get_safe is not None: {self.options.get_safe('myoption') is not None}')",
-                          "self.output.info(f'get_safe == None: {self.options.get_safe('myoption') == None}')",
-                          "self.output.info(f'get_safe != None: {self.options.get_safe('myoption') != None}')" )})
+            .with_package("self.output.info(f'get_safe is None: {self.options.get_safe(\"myoption\") is None}')",
+                          "self.output.info(f'get_safe is not None: {self.options.get_safe(\"myoption\") is not None}')",
+                          "self.output.info(f'get_safe == None: {self.options.get_safe(\"myoption\") == None}')",
+                          "self.output.info(f'get_safe != None: {self.options.get_safe(\"myoption\") != None}')" )})
     tc.run("create .")
     assert "get_safe is None: True" in tc.out
     assert "get_safe is not None: False" in tc.out
